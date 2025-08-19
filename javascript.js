fabde3d1dd3e1253aa17bc6634af75800d609b67
Your Name <you@example.com>
2025-08-19 18:49:09 +0200

    added the possibility to print the operation on the screen

--- javascript.js
+++ javascript.js
@@ -16,16 +16,57 @@ function divide(nb1, nb2){
 
 function operate(nb1, nb2, op){
     switch (op){
-        case add:
+        case "+":
             return add(nb1, nb2);
-        case sub:
+        case "-":
             return sub(nb1, nb2);
-        case mul:
+        case "X":
             return mul(nb1, nb2);
-        case divide:
+        case "/":
             return divide(nb1, nb2);
     }
 
 }
+
+function clear() {
+     const output = document.querySelector('.line1');
+     output.textContent ="";
+}
+
+function main(){
+    const buttons = document.querySelectorAll('button');
+    const output = document.querySelector('.line1')
+    const operators = ["+", "-", "X", "/"];
+    let values = {nb1: "", nb2: ""}
+    let operator;
+    let current = values.nb1;
+
+    buttons.forEach(button => {
+
+        if (button.textContent === "Clear"){
+            button.addEventListener('click', () =>{
+                clear();
+        })}
+        else if(operators.includes(button.textContent)){
+            button.addEventListener('click', () =>{
+                operator = button.textContent;
+                current = values.nb2;
+                output.textContent += ` ${operator} `
+        })}
+        else if(button.textContent === "="){
+            button.addEventListener('click', () =>{
+                let answer = operate(values.nb1, values.nb2, operator);
+                output.textContent += ` = ${answer}`
+        })}
+
+        else{
+            button.addEventListener('click', () =>{
+                output.textContent += `${button.textContent}`
+        })}
+
+    })
+}
+
+main()
 
 console.log(operate(2, 3, mul))
